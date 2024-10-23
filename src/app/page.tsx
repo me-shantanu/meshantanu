0d2738ea0354af8402a58d159196e72944146de2
meShantanu <shantmiitanphy@gmail.com>
2024-10-24 01:12:22 +0530

    configure font size based on tag and screensize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import React, { FC } from 'react'
 
 const Home: FC = () => {
   return (
-    <main>hey this is header</main>
+    <main>hey this is header
+
+      <h1>HeyH1</h1>
+      <h4>Hey h4</h4>
+    </main>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,86 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontSize: {
+        base: '1rem',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [
+    function ({ addBase }: any) {
+      addBase({
+        h1: { fontSize: '1.875rem' },
+        h2: { fontSize: '1.5rem' },
+        h3: { fontSize: '1.25rem' },
+        h4: { fontSize: '1rem' },
+        h5: { fontSize: '0.875rem' },
+        h6: { fontSize: '0.75rem' },
+
+        '@screen sm': {
+          h1: { fontSize: '1.5rem' },
+          h2: { fontSize: '1.25rem' },
+          h3: { fontSize: '1.125rem' },
+          h4: { fontSize: '0.9375rem' },
+          h5: { fontSize: '0.75rem' },
+          h6: { fontSize: '0.625rem' },
+        },
+
+        '@screen md': {
+          h1: { fontSize: '1.625rem' },
+          h2: { fontSize: '1.375rem' },
+          h3: { fontSize: '1.125rem' },
+          h4: { fontSize: '1rem' },
+          h5: { fontSize: '0.8125rem' },
+          h6: { fontSize: '0.6875rem' },
+        },
+
+        '@screen lg': {
+          h1: { fontSize: '2rem' },
+          h2: { fontSize: '1.75rem' },
+          h3: { fontSize: '1.5rem' },
+          h4: { fontSize: '1.0625rem' },
+          h5: { fontSize: '0.875rem' },
+          h6: { fontSize: '0.75rem' },
+        },
+
+        '@screen xl': {
+          h1: { fontSize: '2.25rem' },
+          h2: { fontSize: '2rem' },
+          h3: { fontSize: '1.75rem' },
+          h4: { fontSize: '1.125rem' },
+          h5: { fontSize: '0.9375rem' },
+          h6: { fontSize: '0.8125rem' },
+        },
+
+        '@screen 2xl': {
+          h1: { fontSize: '2.5rem' },
+          h2: { fontSize: '2.25rem' },
+          h3: { fontSize: '2rem' },
+          h4: { fontSize: '1.25rem' },
+          h5: { fontSize: '1rem' },
+          h6: { fontSize: '0.875rem' },
+        },
+      })
+    },
+  ],
+}
+
+export default config
