84313bcb6601582d8e9a56da12f42c8be13ef20d
meShantanu <shantmiitanphy@gmail.com>
2024-11-24 22:21:17 +0530

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,6 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 
 const DEBOUNCE_TIME = 100;
-
-export const isSmallScreen = (): boolean => document.body.clientWidth < 767;
-export const NO_MOTION_PREFERENCE_QUERY =
-  "(prefers-reduced-motion: no-preference)";
-
 export interface IDesktop {
   isDesktop: boolean;
 }
